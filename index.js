c092d99ffcd9985cab0d783313db46cf19f28467
Phil Rukin <philipp@rukin.me>
2020-06-25 18:08:32 +0200

    Add cache

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const polka = require('polka');
 const send = require('@polka/send-type');
+const NodeCache = require('node-cache');
 
+const cache = new NodeCache({ stdTTL: 600 });
 const getTop = require('./drib');
 
 const port = process.env.PORT || 8080;
+
+const getCachedTop = async () => {
+  let top = cache.get('top');
+  if (!top) {
+    top = await getTop();
+    cache.set('top', top);
+  }
+  return top;
+};
 
 polka()
   .get('/', (req, res) => {
     return send(res, 200, 'hello!');
   })
   .get('/top', async (req, res) => {
-    return send(res, 200, {
-      top: await getTop(),
-    });
+    try {
+      return send(res, 200, {
+        top: await getCachedTop(),
+        ok: true,
+      });
+    } catch (error) {
+      return send(res, 500, {
+        error: error.message,
+        ok: false,
+      });
+    }
   })
   .listen(port, (err) => {
     if (err) throw err;
